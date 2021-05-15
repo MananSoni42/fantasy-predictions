7be5815bda8fb5522ec455b77bba71f5408d1bc0
NNFL BITS 2021 <nnflbits2021@gmail.com>
2021-05-15 12:40:11 +0530

    Add script for getting all match urls

--- get_scorecard_points.py
+++ get_scorecard_points.py
@@ -3,6 +3,21 @@ import requests
 from pprint import pprint
 from bs4 import BeautifulSoup
 
+def get_all_match_urls():
+    url = 'https://www.espncricinfo.com/series/ipl-2021-1249214/match-results'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'lxml')
+    matches = []
+    ipl_matches = soup.find('div', {"class": lambda x: x and 'series-results-page-wrapper' in x.split()})
+    for div in ipl_matches.find_all('a', {"class": lambda x: x and 'match-info-link-FIXTURES' in x.split()}):
+        status = div.find('div', {"class": lambda x: x and 'status' in x.split()}).text
+        link = div['href']
+        matches.append({
+            'status': status,
+            'url': url,
+        })
+    return matches
+
 def get_espn_scorecard(match_url, save=False):
 
     r = requests.get(match_url)
@@ -35,9 +50,10 @@ def get_espn_scorecard(match_url, save=False):
                     scorecard['batsman'].append(curr_row)
                 else:
                     scorecard['bowler'].append(curr_row)
-                    
+
     return scorecard
 
 
-scorecard = get_espn_scorecard('https://www.espncricinfo.com/series/ipl-2021-1249214/mumbai-indians-vs-royal-challengers-bangalore-1st-match-1254058/full-scorecard')
-pprint(scorecard)
+pprint(get_all_match_urls())
+#scorecard = get_espn_scorecard('https://www.espncricinfo.com/series/ipl-2021-1249214/mumbai-indians-vs-royal-challengers-bangalore-1st-match-1254058/full-scorecard')
+#pprint(scorecard)
