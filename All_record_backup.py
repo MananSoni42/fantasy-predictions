2e9ff3c8efde4aeed98581dfb53419933a701e10
Manan Soni <manansoni1399@gmail.com>
2021-05-26 22:14:59 +0530

    Add player level data

--- All_record_backup.py
+++ All_record_backup.py
@@ -6,42 +6,101 @@ import requests
 from pprint import pprint
 from bs4 import BeautifulSoup
 from Player_form import *
+from time import sleep
+from utils import clean
+
+def get_role_str(divs):
+    for div in divs:
+        if clean(div.p.text) == 'playing-role':
+            return get_role(div.h5.text)
+    raise Exception('No playing role found')
+
+def get_role(role):
+    role = clean(role).split('-')
+    wk = 'wicketkeeper' in role
+    if 'allrounder' in role:
+        return 'all', wk
+    elif 'batter' in role or 'batsman' in role or 'wicketkeeper' in role:
+        return 'bat', wk
+    elif 'bowler' in role or 'arm' in role or 'legbreak' in role or 'break' in role or 'spin' in role:
+        return 'bowl', wk
+    else:
+        raise Exception(f'No: `{role}`')
 
 def overall_record_backup(overall_url_backup):
     r = requests.get(overall_url_backup)
     soup = BeautifulSoup(r.content, 'lxml')
     overall_record_backup={
-        'player_name':'',
-        'stats':[],
+        'player_name': '',
+        'role': '',
+        'wk': False,
+        'stats-bat': {},
+        'stats-bowl': {},
     }
-    overall_record_backup['player_name']=soup.find('div', {"class":'player-card'}).h2.text
-    j=0
+
+    bat_row = {
+        'test': ['-']*14,
+        't20i': ['-']*14,
+        'odi': ['-']*14,
+        'fc': ['-']*14,
+        'list a': ['-']*14,
+        't20': ['-']*14,
+    }
+    bowl_row = {
+        'test': ['-']*13,
+        't20i': ['-']*13,
+        'odi': ['-']*13,
+        'fc': ['-']*13,
+        'list a': ['-']*13,
+        't20': ['-']*13,
+    }
+
     for table in soup('table'):
-        j=j+1
         if table.get('class',[]) and 'standings-widget-table' in table['class']:
-            title = soup.find('h5', class_='border-bottom-gray-300').get_text()
-            print(title)
-            if(title=='Batting & Fielding'):
+            title = table.parent.parent.find('h5', class_='border-bottom-gray-300').get_text()
+            title = clean(title)
+            #print(f'`{title}`')
+            if title == 'batting-&-fielding':
                 for row in table.tbody('tr'):
-                    curr_row = []
+                    btrow = []
                     for col in row('td'):
-                        curr_row.append(col.text)
-                    overall_record_backup['stats'].append(curr_row)
+                        btrow.append(col.text)
+                    bat_row[clean(btrow[0])] = btrow[1:]
+            elif title == 'bowling':
+                for row in table.tbody('tr'):
+                    bwrow = []
+                    for col in row('td'):
+                        bwrow.append(col.text)
+                    bowl_row[clean(bwrow[0])] = bwrow[1:]
             else:
-                print("Bowling hehe")
-        if(j==1):
-            break
-        
+                raise Exception('No')
+
+    overall_record_backup['player_name']=soup.find('div', {"class":'player-card'}).h2.text
+    role, wk = get_role_str(soup.find('div', {"class":'player_overview-grid'})('div'))
+    overall_record_backup['role'] = role
+    overall_record_backup['wk'] = wk
+    overall_record_backup['stats-bat'] = bat_row
+    overall_record_backup['stats-bowl'] = bowl_row
     return overall_record_backup
-header_player_record_backup=['player-name','format','matches','innings','not-outs','Runs scored','Highest Score','Average','BF','Strike-rate','100s','50s','4s','6s','Catches','Stumpings']
+
+header_player_record_backup=['player-name', 'WK', 'role', 'format'] +\
+    ['matches','innings','not-outs','Runs scored','Highest Score','Average','BF','Strike-rate','100s','50s','4s','6s','Catches','Stumpings'] +\
+    ['Mat', 'Inns', 'Balls', 'Runs', 'Wkts', 'BBI', 'BBM', 'Ave', 'Econ', 'SR', '4w', '5w', '10w']
+
 bat_records_backup=[]
-i=0
-for i in range(193):
-    url_backup = players_url[i]
+
+for i,url_backup in players_url.items():
     records_backup=overall_record_backup(url_backup)
-    for record in records_backup['stats']:
-        bat_records_backup.append([records_backup['player_name'],*record])
-   
+    name = clean(records_backup['player_name'])
+    sleep(0.5)
+    print(i,name, records_backup['role'], 'wk' if records_backup['wk'] else '')
+    for key in records_backup['stats-bat'].keys():
+        row = [key] + records_backup['stats-bat'][key] + records_backup['stats-bowl'][key]
+        bat_records_backup.append([records_backup['player_name'],
+                                   int(records_backup['wk']),
+                                   records_backup['role'],
+                                   *row])
+
 with open('player_batting_record_backup.csv', 'w') as f:
         writer = csv.writer(f)
         writer.writerow(header_player_record_backup)
