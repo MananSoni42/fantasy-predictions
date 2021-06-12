6ad78587ebc1ebe8e65ea125d2c47e44728715c3
swagboy20 <rahulsanjayshah@gmail.com>
2021-06-12 19:00:47 +0530

    eda

--- final_data/data_anal.py
+++ final_data/data_anal.py
@@ -4,24 +4,27 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
 from pprint import pprint
-
+import statistics
 other_path = "data-ipl-fantasy.csv"
 df = pd.read_csv(other_path, header=None)
 new_header = df.iloc[0]
 df = df[1:]
 df.columns = new_header
-print(df.head(5))
+convert_dict = { 'points':float,'batting-runs':float,'wickets':float,'sr':float,'econ':float}
+df=df.astype(convert_dict)
+#print(df.head(5))
 clean = lambda x: x.replace(' ','').replace('\n','')
 names=[]
 names = set(df['player-name'])
-names = set(list(names)[:5])
+print(len(names))
+names = set(list(names)[:25])
 
 player_form = dict()
 n = len(names)
+lolmax=list()
 for i,name in enumerate(names):
-    print(f'{i+1}/{n}')
+    #print(f'{i+1}/{n}')
     hehe=[]
-    #hehe0=[]
     hehe1=[]
     hehe2=[]
     hehe3=[]
@@ -34,18 +37,18 @@ for i,name in enumerate(names):
             hehe1.append(df.loc[i].at["batting-runs"])
             hehe2.append(df.loc[i].at["wickets"])
             hehe3.append(df.loc[i].at["sr"])
-            hehe4.append(df.loc[i].at["econ"])
-            #print(df.loc[i, ['player-name', 'points']])
-    player_form[name] = pd.DataFrame(
-        {'Points': hehe,
-         'Batting-Runs': hehe1,
-         'Wickets': hehe2,
-         'Strike-Rate': hehe3,
-         'Economy': hehe4,
-         #'Player-name':hehe0
-        })
-
-pprint(player_form)
+            hehe4.append(df.loc[i].at["econ"])            
+    av1=statistics.mean(hehe)
+    av2=statistics.mean(hehe1)
+    av3=statistics.mean(hehe2)
+    av4=statistics.mean(hehe3)
+    av5=statistics.mean(hehe4)
+    lolmax.append([name,av1,av2,av3,av4,av5])    
+#print(player_form[axar-patel].groupby('batting').median())
+lolmax=pd.DataFrame(lolmax)
+lolmax.columns=["Player-name","Average Points","Average Batting-Runs","Average wicket per match","Average sr","Average bowl econ"]
+print(lolmax)
+#pprint(player_form)
 
 '''
 player_form = player_form.astype(float)
