35321bdbeb08dd16c34f385609003da068990b08
swagboy20 <rahulsanjayshah@gmail.com>
2021-06-14 10:07:47 +0530

    eda

--- final_data/data_anal.py
+++ final_data/data_anal.py
@@ -16,12 +16,13 @@ df=df.astype(convert_dict)
 clean = lambda x: x.replace(' ','').replace('\n','')
 names=[]
 names = set(df['player-name'])
-print(len(names))
-names = set(list(names)[:25])
+names = set(list(names)[:5])
 
 player_form = dict()
 n = len(names)
 lolmax=list()
+count=list()
+j=0
 for i,name in enumerate(names):
     #print(f'{i+1}/{n}')
     hehe=[]
@@ -29,6 +30,7 @@ for i,name in enumerate(names):
     hehe2=[]
     hehe3=[]
     hehe4=[]
+    c=0
 
     for i in range(1,df.shape[0]+1):
         if(df.loc[i].at["player-name"]==name):
@@ -37,18 +39,21 @@ for i,name in enumerate(names):
             hehe1.append(df.loc[i].at["batting-runs"])
             hehe2.append(df.loc[i].at["wickets"])
             hehe3.append(df.loc[i].at["sr"])
-            hehe4.append(df.loc[i].at["econ"])            
+            hehe4.append(df.loc[i].at["econ"])  
+            c=c+1
     av1=statistics.mean(hehe)
     av2=statistics.mean(hehe1)
     av3=statistics.mean(hehe2)
     av4=statistics.mean(hehe3)
     av5=statistics.mean(hehe4)
-    lolmax.append([name,av1,av2,av3,av4,av5])    
+    count.append(c)
+    lolmax.append([name,av1,av2,av3,av4,av5,count[j]])  
+    j=j+1
 #print(player_form[axar-patel].groupby('batting').median())
 lolmax=pd.DataFrame(lolmax)
-lolmax.columns=["Player-name","Average Points","Average Batting-Runs","Average wicket per match","Average sr","Average bowl econ"]
+lolmax.columns=["Player-name","Average Points","Average Batting-Runs","Average wicket per match","Average sr","Average bowl econ","Matches played"]
 print(lolmax)
-#pprint(player_form)
+
 
 '''
 player_form = player_form.astype(float)
