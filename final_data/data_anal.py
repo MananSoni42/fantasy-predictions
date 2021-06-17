57af04be7e65ec112c6136348eb7c4abb62b6ae7
swagboy20 <rahulsanjayshah@gmail.com>
2021-06-17 11:17:28 +0530

    eda

--- final_data/data_anal.py
+++ final_data/data_anal.py
@@ -28,7 +28,7 @@ df1.columns = new_header1
 
 #list to store player-names for data analysis
 
-convert_dict = { 'points':float,'batting-runs':float,'wickets':float,'sr':float,'econ':float}
+convert_dict = { 'points':float,'batting-runs':float,'wickets':float,'sr':float,'econ':float,'balls':int}
 df=df.astype(convert_dict)
 names=[]
 names = set(df['player-name'])
@@ -50,20 +50,28 @@ for i,name in enumerate(names):
     hehe3=[]
     hehe4=[]
     c=0
-
+    cc=0
     for i in range(1,df.shape[0]+1):
         if(df.loc[i].at["player-name"]==name):
             #hehe0.append(df.loc[i].at["player-name"])
+            if(df.loc[i].at["balls"]==0):
+                cc=cc+1
+            #print(df.loc[i].at["balls"])
             hehe.append(df.loc[i].at["points"])
             hehe1.append(df.loc[i].at["batting-runs"])
             hehe2.append(df.loc[i].at["wickets"])
             hehe3.append(df.loc[i].at["sr"])
             hehe4.append(df.loc[i].at["econ"])  
             c=c+1
+    total=0
+    print(c)
+    print(cc)
+    for ele in range(0, len(hehe3)):
+        total = total + hehe3[ele]
     av1=statistics.mean(hehe)
     av2=statistics.mean(hehe1)
     av3=statistics.mean(hehe2)
-    av4=statistics.mean(hehe3)
+    av4=total/(c-cc)
     av5=statistics.mean(hehe4)
     count.append(c)
     lolmax.append([name,av1,av2,av3,av4,av5,count[j]])  
@@ -71,6 +79,7 @@ for i,name in enumerate(names):
 #print(player_form[axar-patel].groupby('batting').median())
 lolmax=pd.DataFrame(lolmax)
 lolmax.columns=["Player-name","Average Points","Average Batting-Runs","Average wicket per match","Average sr","Average bowl econ","Matches played"]
+lolmax['Average sr'] = lolmax['Average sr'].fillna(0)
 print(lolmax)
 
 print(lolmax.corr())
@@ -97,7 +106,7 @@ print(lolu)
 sns.regplot(x="Average Points", y="Matches played", data=lolmax)
 plt.savefig(os.path.join(dir,f'tpss.png'))
 
-sns.regplot(x="Average Points", y="Average Batting-Runs", data=lolmax)
+sns.regplot(x="Average Points", y="Matches played", data=lolmax)
 plt.savefig(os.path.join(dir,f'tpsss.png'))
 '''
 player_form = player_form.astype(float)
